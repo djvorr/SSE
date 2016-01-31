2befb78640a89438bde3858d92c673c7d37fcc83
David <john.david.vorreiter@gmail.com>
2016-01-31 18:20:10 -0500

    Added multi test methods

--- NuWay/NuWay/Form1.cs
+++ NuWay/NuWay/Form1.cs
@@ -12,19 +12,21 @@ namespace NuWay
         //collection for calculating the total using just the prices
         List<double> prices = new List<double>();
 
-        OleDbConnection conn;
-        OleDbCommand cmd;
-        OleDbDataReader reader;
+        public OleDbConnection conn;
+        public OleDbCommand cmd;
+        public OleDbDataReader reader;
         Tokenizer token;
 
-        string selectString = "SELECT Item, Description, Price FROM NuWay";
+        public static string selectString = "SELECT Item, Description, Price FROM NuWay";
 
         public NuWayOrderForm()
         {
             InitializeComponent();
         }
 
-        //get all the items from the db
+        /// <summary>
+        /// get all the items from the db
+        /// </summary>
         public void ConnectToAccess()
         {
             conn = new OleDbConnection();
@@ -42,8 +44,6 @@ namespace NuWay
                     string st = reader["Item"].ToString() + "-" + reader["Description"] + "  $" + reader["Price"];
                     items.Add(st);
                 }
-
-                fillBoxes();
             }
             catch (Exception ex)
             {
@@ -55,7 +55,9 @@ namespace NuWay
             }
         }
 
-        //filter the items from the db collection and assign to corresponding listbox
+        /// <summary>
+        /// filter the items from the db collection and assign to corresponding listbox
+        /// </summary>
         public void fillBoxes()
         {
             foreach (String item in items)
@@ -71,7 +73,10 @@ namespace NuWay
             }
         }
 
-        //get the total cost by adding up all the prices in the price collection
+        /// <summary>
+        /// get the total cost by adding up all the prices in the price collection
+        /// </summary>
+        /// <returns>double total</returns>
         public double getTotal()
         {
             double total = 0.00;
@@ -82,7 +87,9 @@ namespace NuWay
             return total;
         }
 
-        //set the default selected item in each listbos to the first item in each one
+        /// <summary>
+        /// set the default selected item in each listbos to the first item in each one
+        /// </summary>
         public void SelectedItems()
         {
             if (lbBreakfast.Items.Count > 0)
@@ -97,7 +104,9 @@ namespace NuWay
             zeroOut();
         }
 
-        //govern the values for the totals boxes in the bottom right of the window
+        /// <summary>
+        /// govern the values for the totals boxes in the bottom right of the window
+        /// </summary>
         public void Total()
         {
             if (lbOrder.Items.Count < 1)
@@ -124,7 +133,11 @@ namespace NuWay
             }
         }
 
-        //format the final values to look like prices: XXX.XX
+        /// <summary>
+        /// format the final values to look like prices: XXX.XX
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>formatted cost: String amount</returns>
         public String format(String amount)
         {
             //if amount not blank
@@ -147,7 +160,9 @@ namespace NuWay
             return amount;
         }
 
-        //set the totals boxes in the bottom right of the window to 0.00
+        /// <summary>
+        /// set the totals boxes in the bottom right of the window to 0.00
+        /// </summary>
         public void zeroOut()
         {
             tbSubtotal.Text = "0.00";
@@ -155,64 +170,102 @@ namespace NuWay
             tbTotal.Text = "0.00";
         }
 
-        //on Add Breakfast Button click, add selected breakfast item to Order list
-        private void bBreakfast_Click(object sender, EventArgs e)
+        /// <summary>
+        /// on Add Breakfast Button click, add selected breakfast item to Order list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bBreakfast_Click(object sender, EventArgs e)
         {
             lbOrder.Items.Add(lbBreakfast.SelectedItem);
             Total();
         }
 
-        //on Clear Button click, empty the contents of the Order list
-        private void bClear_Click(object sender, EventArgs e)
+        /// <summary>
+        /// on Clear Button click, empty the contents of the Order list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bClear_Click(object sender, EventArgs e)
         {
             if (lbOrder.Items.Count > 0)
                 lbOrder.Items.Clear();
             Total();
         }
 
-        //on Add Dessert Button click, add selected dessert item to Order list
-        private void bDessert_Click(object sender, EventArgs e)
+        /// <summary>
+        /// on Add Dessert Button click, add selected dessert item to Order list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bDessert_Click(object sender, EventArgs e)
         {
             lbOrder.Items.Add(lbDessert.SelectedItem);
             Total();
         }
 
-        //on Add Drink Button click, add selected drink item to Order list
-        private void bDrinks_Click(object sender, EventArgs e)
+        /// <summary>
+        /// on Add Drink Button click, add selected drink item to Order list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bDrinks_Click(object sender, EventArgs e)
         {
             lbOrder.Items.Add(lbDrinks.SelectedItem);
             Total();
         }
 
-        //on Add Lunch/Dinner Button click, add selected Lunch/Dinner item to Order list
-        private void bLD_Click(object sender, EventArgs e)
+        /// <summary>
+        /// on Add Lunch/Dinner Button click, add selected Lunch/Dinner item to Order list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bLD_Click(object sender, EventArgs e)
         {
             lbOrder.Items.Add(lbLD.SelectedItem);
             Total();
         }
 
-        //on Remove Button click, remove selected item from Order List
-        private void bRemove_Click(object sender, EventArgs e)
+        /// <summary>
+        /// on Remove Button click, remove selected item from Order List
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bRemove_Click(object sender, EventArgs e)
         {
             if (lbOrder.Items.Count > 0)
                 lbOrder.Items.Remove(lbOrder.SelectedItem);
             Total();
         }
 
-        //on Total button click, show message box with final cost
-        private void bTotal_Click(object sender, EventArgs e)
+        /// <summary>
+        /// on Total button click, show message box with final cost
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void bTotal_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Your total comes to $" + tbTotal.Text);
         }
 
-        //on First load of order form window
-        private void NuWayOrderForm_Load(object sender, EventArgs e)
+        /// <summary>
+        /// on First load of order form window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void NuWayOrderForm_Load(object sender, EventArgs e)
         {
             ConnectToAccess();
+            fillBoxes();
             SelectedItems();
             zeroOut();
         }
 
+        /// <summary>
+        /// method for obtaining the listboxes for unit testing
+        /// </summary>
+        /// <param name="listBox"></param>
+        /// <returns>ListBox listBox</returns>
         public ListBox getListBox(String listBox)
         {
             if (listBox == "lbOrder")
@@ -229,6 +282,11 @@ namespace NuWay
                 return null;
         }
 
+        /// <summary>
+        /// method for obtaining the textboxes for unit testing
+        /// </summary>
+        /// <param name="textBox"></param>
+        /// <returns></returns>
         public TextBox getTextBox(String textBox)
         {
             if (textBox == "tbSubtotal")
--- NuWay/NuWayUnitTestProject/UnitTest1.cs
+++ NuWay/NuWayUnitTestProject/UnitTest1.cs
@@ -13,9 +13,34 @@ namespace NuWayUnitTestProject
         {
             NuWayOrderForm form = new NuWayOrderForm();
             form.Show();
+
+        }
+
+        [TestMethod]
+        public void ListBoxesLoad()
+        {
+            NuWayOrderForm form = new NuWayOrderForm();
+            form.Show();
+
+            Assert.IsTrue(form.getListBox("lbBreakfast").Items.Count > 0, "lbBreakfast is empty.");
+            Assert.IsTrue(form.getListBox("lbLD").Items.Count > 0, "lbLD is empty.");
+            Assert.IsTrue(form.getListBox("lbDrinks").Items.Count > 0, "lbDrinks is empty.");
+            Assert.IsTrue(form.getListBox("lbDessert").Items.Count > 0, "lbDesserts is empty.");
+            Assert.IsTrue(form.getListBox("lbOrder").Items.Count == 0, "lbOrder is not empty.");
+        }
+
+        [TestMethod]
+        public void TestTextboxesLoad()
+        {
+            NuWayOrderForm form = new NuWayOrderForm();
+            form.Show();
+
             TextBox tb = form.getTextBox("tbTotal");
-            String zero = "0.00";
-            Assert.AreEqual(tb.Text, zero, "Total not zeroed");
+            Assert.AreEqual(tb.Text, "0.00", "Total not zeroed");
+            tb = form.getTextBox("tbTax");
+            Assert.AreEqual(tb.Text, "0.06", "Tax not zeroed");
+            tb = form.getTextBox("tbSubtotal");
+            Assert.AreEqual(tb.Text, "0.00", "SubTotal not zeroed");
         }
     }
 }
